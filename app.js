f345d7525fbdbc9d7aa5be7c2381a69150292263
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-20 22:01:21 +0200

    Init the project

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // require spotify-web-api-node package here:
+const clientId = '3fb91a0d335c4ba4a800aaefc27967cc';
+const clientSecret = '7cb180e0cb7246f4976b1013574a288b';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  });
 
 const indexRouter = require('./routes/index');
 
- const app = express();
+const app = express();
 
- // view engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
- app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
- app.use('/', indexRouter);
+app.use('/', indexRouter);
 
- // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  res.status(404);
+  res.render('not-found');
 });
 
- // error handler
+// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
 
-   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
 });
 
- module.exports = app;
+module.exports = app;
